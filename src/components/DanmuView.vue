b989e7268390d00091ae9029b1b56e6d1cf2912e
Terry <TerryYoung518@hotmail.com>
2024-04-13 18:01:12 +0800

    Update DanmuView.vue

--- src/components/DanmuView.vue
+++ src/components/DanmuView.vue
@@ -10,7 +10,13 @@ import { onMessage, getBfaceURL } from 'dimsum-chat';
 onMessage((_msg, p) => {
   if(p.type === 'comment') {
     const avatar = p.avatar || (p.platform === "bilibili" && p.uid ? getBfaceURL(p.uid) : undefined);
+    const content = p.getCommentHTML({
+      stickerStyle: "height:80px;margin:5px;",
+      emotStyle: "display:inline-flex;vertical-align:baseline;height:36px;line-height:50px;transform:translateY(4px);"
+    })
   }
+}, {
+  customWsServer: import.meta.env.DEV ? 'ws://localhost:13499/websocket' : undefined
 })
 
 </script>
